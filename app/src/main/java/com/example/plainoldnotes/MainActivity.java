007e9c9a8312bb2e105e14bc6130be23354dbe32
pfieffer <cbpmgr@gmail.com>
2019-01-08 21:22:14 +0545

    Add feature to add sample notes

--- app/src/main/java/com/example/plainoldnotes/MainActivity.java
+++ app/src/main/java/com/example/plainoldnotes/MainActivity.java
@@ -3,20 +3,16 @@ package com.example.plainoldnotes;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.plainoldnotes.model.NoteEntity;
 import com.example.plainoldnotes.ui.NotesAdapter;
-import com.example.plainoldnotes.utilities.SampleData;
 import com.example.plainoldnotes.viewModel.MainViewModel;
 
 import java.util.ArrayList;
@@ -91,10 +87,14 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_add_sample_data) {
+            addSampleData();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void addSampleData() {
+        mViewModel.addSampleData();
     }
 }
--- app/src/main/java/com/example/plainoldnotes/data/AppRepository.java
+++ app/src/main/java/com/example/plainoldnotes/data/AppRepository.java
@@ -1,20 +1,40 @@
 package com.example.plainoldnotes.data;
 
+import android.content.Context;
+import android.widget.AutoCompleteTextView;
+
 import com.example.plainoldnotes.model.NoteEntity;
 import com.example.plainoldnotes.utilities.SampleData;
 
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class AppRepository {
-    private static final AppRepository ourInstance = new AppRepository();
+    private static AppRepository ourInstance;
 
     public List<NoteEntity> mNotes;
+    private AppDatabase mDb;
+    private Executor executor = Executors.newSingleThreadExecutor();
 
-    public static AppRepository getInstance() {
+    public static AppRepository getInstance(Context context) {
+        if (ourInstance == null){
+            ourInstance = new AppRepository(context);
+        }
         return ourInstance;
     }
 
-    private AppRepository() {
+    private AppRepository(Context context) {
         mNotes = SampleData.getNotes();
+        mDb = AppDatabase.getInstance(context);
+    }
+
+    public void addSampleData() {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                mDb.noteDao().insertAllNotes(SampleData.getNotes());
+            }
+        });
     }
 }
