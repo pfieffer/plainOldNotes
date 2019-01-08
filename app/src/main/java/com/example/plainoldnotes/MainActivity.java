2ec7eb243f2398e1cfb0bd89a5244df13ad01816
pfieffer <cbpmgr@gmail.com>
2019-01-08 19:05:59 +0545

    Setup Editor Activity

--- app/src/main/java/com/example/plainoldnotes/MainActivity.java
+++ app/src/main/java/com/example/plainoldnotes/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.plainoldnotes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,6 +22,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +30,12 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.recycler_view)
     RecyclerView mRecyclerView;
+
+    @OnClick(R.id.fab)
+    void fabClickHandler(){
+        Intent intent = new Intent(this, EditorActivity.class);
+        startActivity(intent);
+    }
 
     private List<NoteEntity> notesData = new ArrayList<>();
     private NotesAdapter notesAdapters;
@@ -42,15 +50,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         initRecyclerView();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         notesData.addAll(SampleData.getNotes());
 
