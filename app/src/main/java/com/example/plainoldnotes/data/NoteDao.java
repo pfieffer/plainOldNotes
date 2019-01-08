2e13b3c0e09dd9e7694e0448bef9a5864bcf7ce3
pfieffer <cbpmgr@gmail.com>
2019-01-08 19:23:17 +0545

    Create DAO

---
+++ app/src/main/java/com/example/plainoldnotes/data/NoteDao.java
@@ -0,0 +1,37 @@
+package com.example.plainoldnotes.data;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import com.example.plainoldnotes.model.NoteEntity;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertNote(NoteEntity note);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAllNotes(List<NoteEntity> notes);
+
+    @Delete
+    void deleteNote(NoteEntity noteEntity);
+
+    @Query("SELECT * FROM notes WHERE id= :id")
+    NoteEntity getNoteById(int id);
+
+    @Query("SELECT * FROM notes ORDER BY date DESC")
+    LiveData<List<NoteEntity>> getAll();
+
+    @Query("DELETE FROM notes")
+    int deleteAll();
+
+    @Query("SELECT COUNT(*) FROM notes")
+    int getCount();
+}
