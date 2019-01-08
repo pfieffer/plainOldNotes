52108a8d5dff1464da291a0ec718c37296a25f3f
pfieffer <cbpmgr@gmail.com>
2019-01-08 21:41:50 +0545

    Add Feature to delete all notes

--- app/src/main/java/com/example/plainoldnotes/MainActivity.java
+++ app/src/main/java/com/example/plainoldnotes/MainActivity.java
@@ -97,9 +97,17 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_add_sample_data) {
             addSampleData();
+            return true;
+        } else if (id == R.id.action_delete_all){
+            deleteAllNotes();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteAllNotes() {
+        mViewModel.deleteAllNotes();
     }
 
     private void addSampleData() {
