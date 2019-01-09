a268871f64af2af5d77c110cfe45bbe6f7348b64
pfieffer <cbpmgr@gmail.com>
2019-01-09 15:32:36 +0545

    Add functionality for editing existing notes

--- app/src/main/java/com/example/plainoldnotes/EditorActivity.java
+++ app/src/main/java/com/example/plainoldnotes/EditorActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -33,6 +34,7 @@ public class EditorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_editor);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_check_white_24dp);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         ButterKnife.bind(this);
@@ -63,5 +65,24 @@ public class EditorActivity extends AppCompatActivity {
             mViewModel.loadData(noteId);
 
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            saveAndReturn();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        saveAndReturn();
+    }
+
+    private void saveAndReturn() {
+        mViewModel.saveNote(mEditText.getText().toString());
+        finish();
     }
 }
